cf586837c7d2bfcc5c638921377394227e40f847
Henry Blanchette <henry@monkeyface.com>
2024-09-05 17:57:27 -0400

    example dom manipulation

--- src/App.ml
+++ src/App.ml
@@ -1,1 +1,18 @@
-let _ = Js.log "running app.ml";;
+open Webapi.Dom
+
+(* javascript console *)
+let _ = Js.log "running app.ml"
+
+(* dom manipulation *)
+let thn k oa = Option.bind oa k
+let ( let* ) x f = Option.bind x f
+
+let main () =
+  let e = document |> Document.createElement "div" in
+  let* body = document |> Document.asHtmlDocument |> thn HtmlDocument.body in
+  Element.setInnerText e "this is a div created dynamically by ocaml code!";
+  body |> Element.appendChild e;
+  Some ()
+;;
+
+main ()
